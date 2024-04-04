8b74652f24a171b00b7130b8954fbf125f0bb90f
Pramodh Mereddy <pramodh.mereddy@gmail.com>
2024-04-04 14:59:08 -0500

    testing

--- 3_verify_connectivity/vm_validate_chroma_client.py
+++ 3_verify_connectivity/vm_validate_chroma_client.py
@@ -4,10 +4,10 @@ from chromadb.config import Settings
 
 try:
     port=str(os.environ.get('CDSW_READONLY_PORT', 8100))
+    host=os.environ.get('CDSW_PROJECT_URL','')
     if os.environ.get('CHROMA_AUTH', 'false').lower() == 'true':
         user = os.environ.get('CHROMA_USER', 'admin')
         password = os.environ.get('CHROMA_PASSWORD', 'admin')
-        host=os.environ.get('CDSW_PROJECT_URL','')
         print(f"u:{user}, p:{password}, h:{host}, p:{port}")
         client = chromadb.HttpClient(host=host, port=port, 
             settings=Settings(chroma_client_auth_provider="chromadb.auth.basic.BasicAuthClientProvider",chroma_client_auth_credentials=f"{user}:{password}"))
